ed5d14d0bd10648da54deaa5358e2b722f796b68
Andreas Altendorfer <andreas@altendorfer.at>
2014-08-22 10:51:12 +0200

    Update bucket.rb

--- lib/newrelic-couchbase/instrumentation/bucket.rb
+++ lib/newrelic-couchbase/instrumentation/bucket.rb
@@ -17,18 +17,18 @@
     :stats,
     :run
   ].each do |instruction|
-    add_method_tracer instruction, "Couchbase/Bucket/#{instruction.to_s}"
+    add_method_tracer instruction, "ActiveRecord/Bucket/#{instruction.to_s}"
   end
 
-  add_method_tracer :incr, "Couchbase/Bucket/increment"
-  add_method_tracer :decr, "Couchbase/Bucket/decrement"
+  add_method_tracer :incr, "ActiveRecord/Bucket/increment"
+  add_method_tracer :decr, "ActiveRecord/Bucket/decrement"
 
-  add_method_tracer :cas,                   'Couchbase/Bucket/compare_and_swap'
-  add_method_tracer :compare_and_swap,      'Couchbase/Bucket/compare_and_swap'
-  add_method_tracer :design_docs,           'Couchbase/Bucket/design_docs'
-  add_method_tracer :save_design_doc,       'Couchbase/Bucket/save_design_doc'
-  add_method_tracer :delete_design_doc,     'Couchbase/Bucket/delete_design_doc'
-  add_method_tracer :observe_and_wait,      'Couchbase/Bucket/observe_and_wait'
-  add_method_tracer :create_timer,          'Couchbase/Bucket/create_timer'
-  add_method_tracer :create_periodic_timer, 'Couchbase/Bucket/create_perioidic_timer'
+  add_method_tracer :cas,                   'ActiveRecord/Bucket/compare_and_swap'
+  add_method_tracer :compare_and_swap,      'ActiveRecord/Bucket/compare_and_swap'
+  add_method_tracer :design_docs,           'ActiveRecord/Bucket/design_docs'
+  add_method_tracer :save_design_doc,       'ActiveRecord/Bucket/save_design_doc'
+  add_method_tracer :delete_design_doc,     'ActiveRecord/Bucket/delete_design_doc'
+  add_method_tracer :observe_and_wait,      'ActiveRecord/Bucket/observe_and_wait'
+  add_method_tracer :create_timer,          'ActiveRecord/Bucket/create_timer'
+  add_method_tracer :create_periodic_timer, 'ActiveRecord/Bucket/create_perioidic_timer'
 end
